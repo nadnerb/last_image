5dc7f602f4ff3c3722fb91a76d11b851778da1f0
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 22:34:43 +1000

    Unable to get Dalli sessions in memcache. Heres the workaround

--- app.rb
+++ app.rb
@@ -3,11 +3,10 @@ require 'sinatra'
 require 'dalli'
 require 'statsd'
 require 'resolv'
-require 'rack/session/dalli'
 
-set :cache, Dalli::Client.new
-set :session_store, Dalli::Client.new(nil, :namespace => 'users', :expire_after =>1200)
-use Rack::Session::Dalli, :cache => settings.session_store
+enable :sessions
+set :session_secret, 'last'
+set :session_store, Dalli::Client.new(nil,  :expire_after =>1200)
 
 set :statsd, Statsd.new(Resolv.getaddress('statsd.lastmeme.com'), 8125)
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
@@ -19,6 +18,7 @@ end
 get '/' do
   settings.statsd.increment 'image'
   @image = settings.images.sample
+  settings.session_store.add(session['session_id'], @image)
   erb :index
 end
 
