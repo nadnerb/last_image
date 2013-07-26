5c850cac8fb50092894247f75be1bd86bf05c166
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 14:39:21 +1000

    Do it properly, statsd

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'dalli'
+require 'statsd'
 
 set :cache, Dalli::Client.new
-set :statsd, Statsd.new 'statsd.lastmeme.com', 9125
+set :statsd, Statsd.new('statsd.lastmeme.com', 9125)
 
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
 
