251e8ba3dade99b640959db9bec291962d195024
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 19:08:41 +1000

    address the memcache stats correctly.

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ set :statsd, Statsd.new(Resolv.getaddress('statsd.lastmeme.com'), 8125)
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
 
 after do
-  settings.statsd.gauge 'users', settings.session_store.stats['curr_items']
+  settings.statsd.gauge 'users', settings.session_store.stats.values[0]['curr_items']
 end
 
 get '/' do
