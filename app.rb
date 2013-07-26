bb83442c177388547dda98df35c3f780e2a19e53
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 10:51:51 +1000

    One for breno

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'dalli'
 
 set :cache, Dalli::Client.new
 
-set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
+set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
 
 get '/' do
   @image = settings.images.sample
