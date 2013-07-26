39c5781afe8a3222a2693c44dfa9eacba9653a0d
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 14:48:41 +1000

    Better port this one. My finger missed last time.

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'dalli'
 require 'statsd'
 
 set :cache, Dalli::Client.new
-set :statsd, Statsd.new('statsd.lastmeme.com', 9125)
+set :statsd, Statsd.new('statsd.lastmeme.com', 8125)
 
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
 
