decdb88e8ac0aba79ce4c0d19b644e49b57de68d
Stephen Bartlett <stephenb@rtlett.org>
2013-07-26 15:02:10 +1000

    ip address is best

--- app.rb
+++ app.rb
@@ -2,9 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require 'dalli'
 require 'statsd'
+require 'resolv'
 
 set :cache, Dalli::Client.new
-set :statsd, Statsd.new('statsd.lastmeme.com', 8125)
+set :statsd, Statsd.new(Resolv.getaddress('statsd.lastmeme.com'), 8125)
 
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
 
