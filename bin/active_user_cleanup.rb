c646481be8a899829d9b1e66b6e4fb471b5e1b4e
Stephen Bartlett <stephenb@rtlett.org>
2013-07-27 17:04:27 +1000

    Update statsd after user clean up

--- bin/active_user_cleanup.rb
+++ bin/active_user_cleanup.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'dalli'
+require 'statsd'
+require 'resolv'
 require './lib/active_users'
 
 users= ActiveUsers.new(Dalli::Client.new)
@@ -8,4 +10,5 @@ users= ActiveUsers.new(Dalli::Client.new)
 puts "Number of active users before cleanup: #{users.count}"
 new_count= users.cleanup
 puts "Number of active users after cleanup: #{new_count}"
+Statsd.new(Resolv.getaddress('statsd.lastmeme.com'), 8125).gauge 'users', new_count
 
