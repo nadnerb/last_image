c1f2ed31ea4535ff7cd875c1ef1911e213199ff7
Stephen Bartlett <stephenb@rtlett.org>
2013-07-27 12:43:39 +1000

    More metrics

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'resolv'
 
 enable :sessions
 set :session_secret, 'last'
-set :session_store, Dalli::Client.new(nil, :expire_in =>1200)
+set :session_store, Dalli::Client.new(nil, :expires_in =>180)
 
 set :statsd, Statsd.new(Resolv.getaddress('statsd.lastmeme.com'), 8125)
 set :images, ['http://i.qkme.me/3pk0sc.jpg', 'http://t.qkme.me/3v6soz.jpg', 'http://t.qkme.me/3v8twz.jpg', 'http://t.qkme.me/3ops.jpg']
@@ -17,6 +17,7 @@ end
 
 get '/' do
   settings.statsd.increment 'image'
+  settings.statsd.gauge('views', settings.session_store.incr(session['session_id'] + '-image', 1, nil, 1))
   @image = settings.images.sample
   settings.session_store.add(session['session_id'], @image)
   erb :index
