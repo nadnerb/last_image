8a4162da2d3074f0a257917f8f77aba794f795d5
Brendan Spinks <brendanspinks@gmail.com>
2013-07-30 15:50:30 +1000

    continuous image

--- app.rb
+++ app.rb
@@ -7,13 +7,7 @@ before do
 end
 
 set :images, [
-  {:id => 1, :url => 'http://i.qkme.me/3rbofl.jpg'},
-  {:id => 2, :url => 'http://t.qkme.me/3v8twz.jpg'},
-  {:id => 3, :url => 'http://t.qkme.me/3ops.jpg'},
-  {:id => 4, :url => 'http://i.qkme.me/3qqutc.jpg'},
-  {:id => 5, :url => 'http://media2.policymic.com/a99dd6b0533046d442ef72c2c55ae755.jpg'},
-  {:id => 6, :url => 'http://media2.policymic.com/af9daec128ff5f479ee6e6031fec40a4.jpg'},
-  {:id => 7, :url => 'http://media2.policymic.com/842c85f608b4795d6f079a16e9a3c109.jpeg'}
+  {:url => 'img/continuous.jpg'},
 ]
 
 configure do
