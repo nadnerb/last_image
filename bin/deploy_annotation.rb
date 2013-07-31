9cdff3e3d2c484edb9bbe5070183d4b309395145
Stephen Bartlett <stephenb@rtlett.org>
2013-07-31 11:14:04 +1000

    my bad

--- bin/deploy_annotation.rb
+++ bin/deploy_annotation.rb
@@ -2,6 +2,6 @@
 
 require 'librato/metrics'
 Librato::Metrics.authenticate ENV['LIBRATO_EMAIL'], ENV['LIBRATO_KEY']
-Librato::Metrics.annotate :deployments,  "Build #{ENV['BUILD_NUMBER']}"
+Librato::Metrics.annotate :deployments,  "Build #{ENV['BUILD_NUMBER']}",
   :start_time => ARGV[0],
   :end_time => ARGV[1]
