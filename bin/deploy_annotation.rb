0f31c1e36f59a4bd8d03c08c6bf03354cab95dfb
Stephen Bartlett <stephenb@rtlett.org>
2013-07-31 10:53:16 +1000

    provide a description

--- bin/deploy_annotation.rb
+++ bin/deploy_annotation.rb
@@ -2,4 +2,7 @@
 
 require 'librato/metrics'
 Librato::Metrics.authenticate ENV['LIBRATO_EMAIL'], ENV['LIBRATO_KEY']
-Librato::Metrics.annotate :deployments,  :start_time => ARGV[0], :end_time => ARGV[1], "Deployed build #{ENV['BUILD_NUMBER']}"
+Librato::Metrics.annotate :deployments, 
+  :start_time => ARGV[0],
+  :end_time => ARGV[1],
+  :description => "Deployed build #{ENV['BUILD_NUMBER']}"
