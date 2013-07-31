581fd06038ed7c3c4c1158965772a43ce7ee3d38
Stephen Bartlett <stephenb@rtlett.org>
2013-07-31 11:59:59 +1000

    Remove dates from the annotation

--- bin/deploy_annotation.rb
+++ bin/deploy_annotation.rb
@@ -2,6 +2,4 @@
 
 require 'librato/metrics'
 Librato::Metrics.authenticate ENV['LIBRATO_EMAIL'], ENV['LIBRATO_KEY']
-Librato::Metrics.annotate :deployments,  "Build #{ENV['BUILD_NUMBER']}",
-  :start_time => ARGV[0],
-  :end_time => ARGV[1]
+Librato::Metrics.annotate :deployments, "Build #{ENV['BUILD_NUMBER']}"
